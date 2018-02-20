04dbe3c94b8d19abba443421a6db8624d05a9732
bmsch98 <brettschultz98@gmail.com>
2018-02-20 13:33:45 -0500

    update commit

--- STEP101/assets/js/app.js
+++ STEP101/assets/js/app.js
@@ -1,42 +1,49 @@
-	function init() {
-		window.addEventListener('scroll', function(e){
-			var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-				shrinkOn = 300,
-				header = document.querySelector("header");
-			if (distanceY > shrinkOn) {
-				classie.add(header,"smaller");
-			} else {
-				if (classie.has(header,"smaller")) {
-					classie.remove(header,"smaller");
-				}
-			}
-		});
-		
-		$.ajax({
-			
-			method: 'GET',
-			url: 'assets/data/menu.json',
-			dataType: 'json',
-			succes: function (data){
-				console.log('call was good');
-				console.log(data.menu.length);
-				
-				if(data.menu.length >0){
-					data.menu.forEach(function(item){
-						
-						console.log(item);
-						
-						$('nav').append('<a href="'+item.menuLink+'">'+item.menuName+'</a>')
-						
-					})
-				}
-				
-			},
-			error: function(){
-				console.log('call was bad');
-			}
-			
-		});
-	}
+function init() {
 
-	window.onload = init();
+
+    window.addEventListener('scroll', function (e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header, "smaller");
+        } else {
+            if (classie.has(header, "smaller")) {
+                classie.remove(header, "smaller");
+            }
+        }
+    });
+
+
+    $.ajax({
+        method: 'GET',
+        url: 'assets/data/menu.json',
+        dataType: 'json',
+        success: function (data) {
+            console.log('all good');
+            console.log(data.menu.length);
+
+            if (data.menu.length > 0) {
+                data.menu.forEach(function (item) {
+
+                    console.log(item);
+
+                    $('nav').append('<a href="' + item.MenuLink + '">' + item.MenuName + '</a>');
+
+                });
+            }
+
+
+        },
+        error: function () {
+            console.log('bad');
+        }
+
+    });
+
+
+
+
+}
+
+window.onload = init();
